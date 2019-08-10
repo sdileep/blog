b9594a28afef268b71b431203f2467bbe35dd009
Dileep Shiva <sdileep@gmail.com>
2019-08-10 22:08:56 +1000

    update config to use blog's URL

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   author: 'Dileep Shiva',
   githubUrl: 'https://github.com/sdileep/blog',
   // replace this by the url where your website will be published
-  siteUrl: 'http://localhost:8000',
+  siteUrl: 'https://dileeps.netlify.com/',
   social: {
     // leave the social media you do not want to appear as empty strings
     twitter: '',
